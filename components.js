a78b4c30b0729153e01ac1a6763b9db93ac88eb5
Nyctef <nyctef@nyctef.com>
2024-03-30 12:24:34 +0000

    sketch out fuzzy search and first contents page (barebones)

--- components.js
+++ components.js
@@ -16,4 +16,25 @@
     }
   }
   window.customElements.define("centered-text", CenteredText);
+
+  class YoutubeVideo extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      const id = this.getAttribute("data-id");
+      this.shadowRoot.innerHTML = `
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/${id}"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    allowfullscreen
+                ></iframe>
+            `;
+    }
+  }
+  window.customElements.define("youtube-video", YoutubeVideo);
 })();
